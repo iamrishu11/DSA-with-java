89c947e1572e0c466a000e563ecf48a9b657ef1d
Rishank Jain <rishankj749@gmail.com>
2024-08-28 23:59:40 +0530

    Create binarySearch.java

---
+++ binarySearch.java
@@ -0,0 +1,15 @@
+public int binarySearch(int[] arr, int target) {
+    int left = 0, right = arr.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
