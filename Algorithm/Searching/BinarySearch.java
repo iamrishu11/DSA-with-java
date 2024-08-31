972decb8a839e5da41aed5d14ac185542ea04ddf
Rishank Jain <rishankj749@gmail.com>
2024-08-31 17:00:58 +0530

    Created BinarySearch.java

---
+++ Algorithm/Searching/BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not present in array");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
