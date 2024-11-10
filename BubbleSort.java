3858ebdcc696f2335fc2379eaa3b5133399c492f
Rishank Jain <rishankj749@gmail.com>
2024-11-10 23:59:49 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                   arr[j + 1] = temp;
                     swapped = true;
                 }
             }
