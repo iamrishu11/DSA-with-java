38af54a0e8012805c252669421ccff72e695dbc7
Rishank Jain <rishankj749@gmail.com>
2024-11-12 23:55:29 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,6 +13,7 @@ public class BubbleSort {
             for (int j = 0; j < n - i - 1; j++) {
                 // Swap if the element found is greater than the next element
                 if (arr[j] > arr[j + 1]) {
+                    
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                    arr[j + 1] = temp;
