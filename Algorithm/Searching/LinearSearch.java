8b17565d480377971a21a95813a97c57d89a9960
Rishank Jain <rishankj749@gmail.com>
2024-11-18 23:24:36 +0530

    Update LinearSearch.java

--- Algorithm/Searching/LinearSearch.java
+++ Algorithm/Searching/LinearSearch.java
@@ -6,7 +6,7 @@ public class LinearSearch {
         for (int i = 0; i < arr.length; i++) {
             // Check if the current element matches the target
             if (arr[i] == target) {
-                return i; // Return the index of the target element
+                return i; // Return the index of the target element 
             }
         }
         return -1; // Return -1 if the target is not found
