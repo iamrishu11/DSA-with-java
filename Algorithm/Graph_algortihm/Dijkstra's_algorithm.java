b7d878bd8a8e7676943174ce399d517eca21c52a
Rishank Jain <rishankj749@gmail.com>
2024-11-23 23:28:28 +0530

    Update Dijkstra's_algorithm.java

--- Algorithm/Graph_algortihm/Dijkstra's_algorithm.java
+++ Algorithm/Graph_algortihm/Dijkstra's_algorithm.java
@@ -7,7 +7,7 @@ public class DijkstraAlgorithm {
     static class Node implements Comparable<Node> {
         int vertex;
         int distance;
-
+ 
         Node(int vertex, int distance) {
             this.vertex = vertex;
             this.distance = distance;
