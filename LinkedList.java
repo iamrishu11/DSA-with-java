576f53d1803523cb1f8779611efa668f625cb423
Rishank Jain <rishankj749@gmail.com>
2025-01-25 23:28:34 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -4,7 +4,7 @@ public class LinkedList {
     private static class Node {
         int data;
         Node next;  
-          
+           
         Node(int data) {
             this.data = data; 
             this.next = null;  
