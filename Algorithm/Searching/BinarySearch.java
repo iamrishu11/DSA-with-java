4b3ec9f1958b8fb2b075117abc09d05c1c19bd19
Rishank Jain <rishankj749@gmail.com>
2025-04-11 23:59:15 +0530

    Update BinarySearch.java

--- Algorithm/Searching/BinarySearch.java
+++ Algorithm/Searching/BinarySearch.java
@@ -1,6 +1,6 @@
 public class BinarySearch {
     public static int binarySearch(int[] arr, int target) {
-        int left = 0;
+        int left = 0; 
         int right = arr.length - 1;
 
         while (left <= right) {
