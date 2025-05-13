2de94ab2d435a9115da665dd09dea929816fa7bd
Rishank Jain <rishankj749@gmail.com>
2025-05-13 21:01:37 +0530

    Update LinearSearch.java

--- Algorithm/Searching/LinearSearch.java
+++ Algorithm/Searching/LinearSearch.java
@@ -1,7 +1,7 @@
 public class LinearSearch {
 
     // Method to perform linear search
-    public static int linearSearch(int[] arr, int target) {
+    public static int linearSearch(int[] arr, int target) { 
         // Iterate through each element in the array
         for (int i = 0; i < arr.length; i++) {
             // Check if the current element matches the target
